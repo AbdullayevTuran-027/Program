87493cbcedfaa0c5fd84f99095ed0a0a52d79a5b
Elşad Usubov <elshad.usubov.e@student.aztu.edu.az>
2022-03-03 20:31:53 -0800

    Program.cs file created

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ay = "gjgjgkj";
+            if (ay == "Yanvar" || ay == "Dekabr" || ay == "Fevral")
+            {
+                Console.WriteLine("Qis Feslidi");
+            }
+            else if (ay == "Mart" || ay == "Aprel" || ay == "May")
+            {
+                Console.WriteLine("Yaz Feslidi");
+            }
+            else if (ay == "Iyun" || ay == "Iyul" || ay == "Avqust")
+            {
+                Console.WriteLine("Yay Feslidi");
+            }
+            else if (ay == "Sentyabr" || ay == "Oktyabr" || ay == "Noyabr")
+            {
+                Console.WriteLine("Payiz feslidi");
+            }
+            else
+            {
+                Console.WriteLine("Bele ay yoxu");
+            }
+        }
+    }
+}
